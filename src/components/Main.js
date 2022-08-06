ee1f2fd34e8457849418144eb17b84f9433c2071
boon2187 <fumiya.mai.ito@gmail.com>
2022-08-06 18:45:18 +0900

    ドラッグ可能エリアまで設定

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import dummyData from "../dummyData";
 
 const Main = () => {
@@ -9,7 +9,21 @@ const Main = () => {
         <DragDropContext>
             <div className="trello">
                 {data.map((section) => (
-                    
+                    <Droppable key={section.id} droppableId={section.id}>
+                        {(provided) => 
+                            <div className="trello-section" ref={provided.innerRef} {...provided.droppableProps}>
+                                <div className="trello-sction-title">{section.title}</div>
+                                <div className="trello-seciton-content">
+                                    {section.tasks.map((task, index) => (
+                                        <Draggable>
+                                            <div>
+                                                <Card></Card>
+                                            </div>
+                                        </Draggable>
+                                    ))}
+                                </div>       
+                            </div>}
+                    </Droppable>
                 ))}
             </div>
         </DragDropContext>
